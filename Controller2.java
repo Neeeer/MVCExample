648a0f1b203665fab2cd05fb393ecf94c6d6e876
RafalCS <78859260+RafalCS@users.noreply.github.com>
2021-02-10 15:46:51 +0000

    Update Controller2.java

--- Controller2.java
+++ Controller2.java
@@ -20,14 +20,14 @@ public class Controller2 extends JFrame
     private JButton refreshViews; // To prompt them to refresh their contents from the model
  
     // Constructor
-    public Controller2(Model model) {
+    public Controller2(Model model, String title, int size) {
     
         // Record reference to the model
         this.model = model;
         
         // Configure the window
-        setTitle("Controller2");
-        setLocation(40,200);
+        setTitle(title);
+        setLocation(40,size);
         setSize(350,150);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         Container window = getContentPane();
